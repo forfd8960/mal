51f9629c68f4624b9e057e6b36f970d2bd2c595e
forfd8960 <forfd8960@gmail.com>
2024-09-21 12:06:48 +0800

    feaat: read line from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
+use std::io::{self, BufRead, Write};
+
+fn main() -> anyhow::Result<()> {
     println!("Make a Lisp");
+
+    let mut stdin = io::stdin().lock();
+    loop {
+        print!(">> ");
+        io::stdout().flush()?;
+
+        let mut buf = String::new();
+        stdin.read_line(&mut buf)?;
+        println!(">> {}", buf);
+
+        let trim_buf = buf.trim();
+        if trim_buf == "q" || trim_buf == "quit" {
+            break;
+        }
+    }
+    Ok(())
 }
